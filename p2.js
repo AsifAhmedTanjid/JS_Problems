2082d710d1db4a79e6f1e6ba827ca770173d2586
AsifAhmedTanjid <asifahmedtanjid@gmail.com>
2026-01-08 12:29:46 +0600

    problem 2: count vowels in a string

---
+++ p2.js
@@ -0,0 +1,16 @@
+const CountVowelFunc =(str)=>{
+    str= str.toLowerCase();
+    let count=0;
+    for(let i=0;i<str.length;i++){
+        if(str[i]==="a"||str[i]==="e"||str[i]==="i"||str[i]==="o"||str[i]==="u"){
+            count++;
+        }
+    }
+    console.log(count);
+    
+    return count;
+
+}
+
+const str="programming";
+CountVowelFunc(str);
