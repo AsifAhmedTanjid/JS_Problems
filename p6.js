69a6764fd7e2166ed8859939b914109b5df093df
AsifAhmedTanjid <asifahmedtanjid@gmail.com>
2026-01-08 14:03:46 +0600

    Problem 7: Find Even Numbers in an Array

---
+++ p6.js
@@ -0,0 +1,13 @@
+const evenFunc=(arr)=>{
+    let evenArr=[];
+    for(let i=0;i<arr.length;i++){
+       if(arr[i]%2===0){
+        evenArr.push(arr[i]);
+       }
+    }
+    return evenArr;
+}
+
+const arr=  [1, 2, 3, 4, 5, 6]
+console.log(evenFunc(arr));
+
